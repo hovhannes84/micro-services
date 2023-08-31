a4c6a3ac618e7d7d87662ac3718728a3fd37810e
Armensargsyan76 <76764242az@gmail.com>
2023-08-31 12:40:06 +0400

    Initial commit

---
+++ school/src/main/java/com/example/school/SchoolApplication.java
@@ -0,0 +1,16 @@
+package com.example.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+
+}
